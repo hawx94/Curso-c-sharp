df03138d0168dd0cd673299eb81e2d17c3bf3af0
Thiago Correia de Souza <thiago_c_souza@hotmail.com>
2022-02-17 09:18:08 -0300

    Vetor
    add exercício de vetor

---
+++ Vetor/Vetor/Apartamentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetor
+{
+    class Apartamentos
+    {
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public int NumeroDoQuarto { get; set; }
+
+
+        public override string ToString()
+        {
+            return NumeroDoQuarto
+                + ": "
+                + Nome
+                + ", "
+                + EMail;
+        }
+    }
+}
+
---
+++ Vetor/Vetor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apartamentos[] vectAp = new Apartamentos[10];
+            Console.WriteLine("Número de estudantes: ");
+            int NumeroDeEstudantes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < NumeroDeEstudantes; i++)
+            {
+                Console.WriteLine("Nome do aluno: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Email do aluno: ");
+                string email = Console.ReadLine();
+                Console.WriteLine("Número do quarto: ");
+                int numeroDoQuarto = int.Parse(Console.ReadLine());
+                vectAp[numeroDoQuarto] = new Apartamentos { Nome = nome, EMail = email, NumeroDoQuarto = numeroDoQuarto };
+            }
+
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vectAp[i] != null)
+                {
+                    Console.WriteLine(vectAp[i]);
+                }
+            }
+        }
+    }
+}
