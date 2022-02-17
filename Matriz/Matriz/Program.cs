7e7796b447ad862378ec33a1b9f81a149c31fbd2
Thiago Correia de Souza <thiago_c_souza@hotmail.com>
2022-02-17 09:29:05 -0300

    matriz
    iniciado exercício de matriz

---
+++ Matriz/Matriz/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de linhas da Matriz");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o número de linhas da Matriz");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[m, n];
+
+
+        }
+    }
+}
