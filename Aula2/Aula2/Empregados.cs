159495a2f7592ab0511f14f2fb24c2c671b932c4
Thiago Correia de Souza <thiago_c_souza@hotmail.com>
2022-02-17 09:15:36 -0300

    aula de lista
    Add aula de lista

---
+++ Aula2/Aula2/Empregados.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula
+{
+    class Empregados
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salario { get; set; }
+
+        public Empregados(int id,string name, double salario)
+        {
+            Id = id;
+            Name = name;
+            Salario = salario;
+        }
+
+
+        public void AumentoDeSalarioDofuncionario(double porcentagem)
+        {
+            Salario += Salario * porcentagem / 100;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Salario.ToString("F2");
+
+
+        }
+
+
+    }
+}
---
+++ Aula2/Aula2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Empregados> listaDeEmpregados = new List<Empregados>();
+            
+            
+            Console.WriteLine("Quantos empregados serão registrados");
+            int numeroDeEmpregados = int.Parse(Console.ReadLine());
+            for(int i = 0; i < numeroDeEmpregados; i++)
+            {
+                Console.WriteLine("Empregado " + (i+1) + ":");
+                Console.WriteLine("Empregado id: " );
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Empregado Nome: ");
+                string name = Console.ReadLine( );
+                Console.WriteLine("Empregado Salario: ");
+                double salario = double.Parse(Console.ReadLine());
+                listaDeEmpregados.Add(new Empregados(id,name,salario));
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("Qua o id do empregado que receberá um aumento: ");
+            int idAumento = int.Parse(Console.ReadLine());
+
+            Empregados emp = listaDeEmpregados.Find(x => x.Id == idAumento);
+            if(emp != null)
+            {
+                Console.WriteLine("Valor do aumento em %: ");
+                double porcentagem = double.Parse(Console.ReadLine());
+                emp.AumentoDeSalarioDofuncionario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Id nao existente!!!");
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada :");
+            foreach (Empregados obj in listaDeEmpregados)
+            {
+                Console.WriteLine(obj);
+            }
+
+        }
+    }
+}
